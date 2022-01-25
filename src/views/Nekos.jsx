590e59a7c684aea1fc5ac559dd9ba48485bfa2bb
ckaynatividad <natividad.ckay@gmail.com>
2022-01-24 18:35:20 -0800

    commiting neko list to see if rendered in local

---
+++ src/views/Nekos.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import NekoList from '../components/NekoList';
+import { getNekos } from '../services/nekos';
+
+export default function Nekos() {
+    const [nekos, setNekos] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getNekos();
+            setNekos(data);
+        };
+        fetchData();
+    }, []);
+  return (
+  <div>
+<NekoList nekos={nekos} />
+  </div>);
+}
