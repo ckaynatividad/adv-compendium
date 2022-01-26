2c974bd26723eb76d369e3f9853105655d335c6e
ckaynatividad <natividad.ckay@gmail.com>
2022-01-25 18:44:37 -0800

    added s to http

--- src/services/makeups.js
+++ src/services/makeups.js
@@ -2,7 +2,7 @@ export async function getMakeups(query) {
   const params = new URLSearchParams();
   params.set('makeups', query);
   const resp = await fetch(
-    'http://makeup-api.herokuapp.com/api/v1/products.json'
+    'https://makeup-api.herokuapp.com/api/v1/products.json'
   );
   const data = await resp.json();
  
@@ -11,7 +11,7 @@ export async function getMakeups(query) {
 
 export async function searchBrands(name) {
   const resp = await fetch(
-    `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${name}`
+    `https://makeup-api.herokuapp.com/api/v1/products.json?brand=${name}`
   );
   const data = await resp.json();
   return data;
